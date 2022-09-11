acd145a9cb2ad4f790f4ad2d78c353c59f71c76d
Miller <farwes1180@gmail.com>
2022-09-11 20:00:31 +0900

    arrays

--- variables_constants/array.cpp
+++ variables_constants/array.cpp
@@ -4,8 +4,29 @@
  * Arrays = multiple values inside one variables
  */
 
-void arrays () {
+#include <array>
 
+// namespaces
+using namespace std;
 
-  qDebug() << "";
+void arrays() {
+  // Zero based index array
+  int ages[4] = {23, 7, 10, 11};
+
+  for (int age: ages) {
+    qInfo() << age;
+  }
+
+  qInfo() << "ages" << ages;// it gets arrays position in memory
+
+  array<int, 5> cars;
+
+  for (int i = 0; i < 5; i++) {
+    cars[i] = i * 10;
+    qInfo() << cars[i];
+  }
+
+  qInfo() << "Size : " << cars.size(); // gets arrays size
+  qInfo() << "sizeof : " << sizeof(cars);
+  qInfo() << "Last : " << cars[cars.size() - 1];
 }
