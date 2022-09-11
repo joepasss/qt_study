0916d204bd4196050dd4fc8f7cb33aa62281b8c6
Miller <farwes1180@gmail.com>
2022-09-12 00:30:49 +0900

    logical operators

--- variables_constants/logical_op.cpp
+++ variables_constants/logical_op.cpp
@@ -1,11 +1,19 @@
 // Comparison & logical operators
 // compares different variables
 // x == y, x < y, x <= y, x != y, etc ...
+// logical operation
+// &&, ||, !
+// logical and &&
+// logical or ||
+// logical not !
 
 void logical() {
   int a = 10;
   int b = 12;
 
-  qInfo() << "" << (a == b);
-  qInfo() << "" << (a < b);
+  qInfo() << "" << (a == b);            // false
+  qInfo() << "" << (a < b);             // true
+  qInfo() << "" << (a == b && a != b);  // false
+  qInfo() << "" << (a == b || a != b);  // true
+  qInfo() << "" << (a != b);            // true
 }
